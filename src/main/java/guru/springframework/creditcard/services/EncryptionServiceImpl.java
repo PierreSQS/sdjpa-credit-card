910fa724c172a7483f8c8ac619d9e49e982c8880
PierreSQS <nomo_messina@web.de>
2022-07-27 15:55:43 +0200

    Sect22_Chap192-Implement Encryption Interceptor: related code

--- src/main/java/guru/springframework/creditcard/services/EncryptionServiceImpl.java
+++ src/main/java/guru/springframework/creditcard/services/EncryptionServiceImpl.java
@@ -3,7 +3,6 @@ package guru.springframework.creditcard.services;
 import org.springframework.stereotype.Service;
 
 import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
 import java.util.Base64;
 
 @Service
@@ -16,6 +15,6 @@ public class EncryptionServiceImpl implements EncryptionService{
 
     @Override
     public String decrypt(String encryptedText) {
-        return Arrays.toString(Base64.getDecoder().decode(encryptedText));
+        return new String(Base64.getDecoder().decode(encryptedText));
     }
 }
